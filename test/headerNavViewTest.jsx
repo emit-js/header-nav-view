d5b99eb9fa87e19893ff7ccc00ab658fa324e9cf
winton <mail@wintoni.us>
2019-02-25 22:00:40 -0500

    Better test paths

--- test/headerNavViewTest.jsx
+++ test/headerNavViewTest.jsx
@@ -1,10 +1,10 @@
 /* eslint-env jest */
 /** @jsx dot.el */
 
-var dot = require("../dist/testDot")()
+var dot = require("./page/testDot")()
 
 require("@dot-event/render")(dot)
-require("../dist/testPage")(dot)
+require("./page/testPage")(dot)
 
 test("build headerNavView test page", function() {
   return dot.render({
