a37971fc7d38c25fa703b5dfcf518b1690c8b57e
winton <mail@wintoni.us>
2019-03-10 23:11:01 -0400

    Updated return check and opts handling

--- headerNavView.jsx
+++ headerNavView.jsx
@@ -1,10 +1,15 @@
 /** @jsx dot.el */
 
 module.exports = function(dot) {
+  if (dot.headerNavView) {
+    return
+  }
+
   require("./headerNavMenuView")(dot)
 
   dot.any("headerNavViewRender", headerNavViewRender)
   dot.any("headerNavViewUpdate", headerNavViewUpdate)
+
   dot.view("headerNavView")
 }
 
